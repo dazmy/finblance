e9fe5f393caf4c3e23faee86b222b93349bbed72
dazmy <adamzamzam571@gmail.com>
2024-11-27 22:29:46 +0700

    input jenis nabung can set percent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { CardComponent } from './components/card/card.component';
 import { DecimalDirective } from './directives/decimal.directive';
 import { FormArray, FormControl, ReactiveFormsModule } from '@angular/forms';
 
+interface PercentSaving {
+  name?: string;
+  percent?: number;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,15 +18,28 @@ import { FormArray, FormControl, ReactiveFormsModule } from '@angular/forms';
 export class AppComponent {
   money = new FormControl('');
   typeSaving = new FormControl('');
-  listTypeSaving = new FormArray<FormControl>([]);
+  listTypeSaving = new FormArray<FormControl<PercentSaving | null>>([]);
 
   addTypeSaving() {
     if (!this.typeSaving.value || !this.money.value) {
       return;
     }
-    const control = new FormControl(this.typeSaving.value);
+
+    const data: PercentSaving = {
+      name: this.typeSaving.value,
+      percent: 0,
+    }
+    const control = new FormControl(data);
     this.listTypeSaving.push(control);
     this.typeSaving.setValue('');
+  }
+
+  setPercent(event: Event, index: number) {
+    const inputElement = event.target as HTMLInputElement;
+    const control = this.listTypeSaving.at(index);
+    if (control && control.value) {
+      control.value.percent = parseInt(inputElement.value);
+    }
   }
 
   deleteTypeSaving(index: number) {
